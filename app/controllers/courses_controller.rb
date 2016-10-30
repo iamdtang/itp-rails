cd2b2200d3bcd9c291fddee9770084478f8d5fe3
David Tang <dtang859@gmail.com>
2016-10-30 11:07:10 -0700

    wip

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,8 @@ class CoursesController < ApplicationController
   def index
     @terms = Term.order(id: :desc).all()
   end
+
+  def show
+    @term = Term.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/courses', to: 'courses#index'
+  get '/courses', to: 'courses#index', as: 'courses'
+  get '/courses/:id', to: 'courses#show', as: 'course'
 end
